dad069f146fd9f8b2ae82d1fec62a63751ae2849
David Warnock <50368515+verticaldepth@users.noreply.github.com>
2021-06-14 18:46:20 +0100

    Adds stubbed out pre-demo version

---
+++ app/src/main/java/com/davewarnock/demoapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.davewarnock.demoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.davewarnock.demoapp.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
